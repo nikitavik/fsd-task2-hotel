a04a7755ae6e16ffdb3d6bf07c7089a41b114697
Nikita <nikitakornilov1998@gmail.com>
2020-12-22 00:16:39 +0300

    "fixed some problems in webpack.config.js"

--- webpack.config.js
+++ webpack.config.js
@@ -2,24 +2,33 @@ const path =require("path")
 const HTMLWebpackPlugin = require("html-webpack-plugin")
 const {CleanWebpackPlugin} = require("clean-webpack-plugin")
 const miniCssExtractPlugin = require("mini-css-extract-plugin")
+const CopyPlugin = require("copy-webpack-plugin")
 module.exports = {
     context: path.resolve(__dirname, "src"),
     mode: "development",
-    entry: "./index.js",
+    entry: {
+        main: "./index.js"
+    },
     output: {
         filename: "[name].bundle.js",
-        path: path.resolve(__dirname, "dist")
+        path: path.resolve(__dirname, "dist"),
+        publicPath: '',
     },
     plugins: [
         new HTMLWebpackPlugin({
             template: "index.pug",
-            inject: true
+            inject: true,
         }),
         new CleanWebpackPlugin(),
         new miniCssExtractPlugin({
             filename: "[name].css"
 
-        })
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: path.resolve(__dirname,"src/img"), to: path.resolve(__dirname,"dist/img") },
+            ],
+        }),
 
     ],
     module: {
