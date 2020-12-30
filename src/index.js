a9f0da90b198aa5dc430cf78c0ea3ff4aaed0cab
Nikita <nikitakornilov1998@gmail.com>
2020-12-30 03:14:06 +0300

    made structure and code cleaner

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "/styles/reset.scss"
 import "/styles/main.scss"
+import "/styles/colors-and-types.scss"
 import "/components/logo/logo.scss"
 import "/components/header/header.scss"
 import "/components/colors-and-types/color-block/color-block.scss"
 import "/components/colors-and-types/type-block/type-block.scss"
-import "/components/colors-and-types/colors-and-types.scss"
