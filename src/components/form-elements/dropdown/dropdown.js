26c7ee9661c3a5ad2362c4b6d0165b05bbfd9adc
Nikita <nikitakornilov1998@gmail.com>
2021-02-21 04:52:50 +0300

    dropdown is already done

--- src/components/form-elements/dropdown/dropdown.js
+++ src/components/form-elements/dropdown/dropdown.js
@@ -1,20 +1,34 @@
 $(document).ready(function (){
-    // $(".dropdown__field").click(function () {
-    //     $(".dropdown__options").slideToggle()
-    //     $(".dropdown__field").toggleClass("expand")
-    // })
+    $(".dropdown__field").click(function () {
+        $(".dropdown__options").slideToggle()
+        $(".dropdown__field").toggleClass("expand")
+    })
 
+    const counters = document.getElementsByClassName("dropdown__counter")
+    const plus = document.getElementsByClassName("dropdown__plus")
+    const minus = document.getElementsByClassName("dropdown__minus")
+    const clear = document.getElementsByClassName("d-clear")
     const dropdown = {
-        open: ()=>{
-            $(".dropdown__options").slideToggle()
-            $(".dropdown__field").toggleClass("expand")
+        plus :function (option) {
+            let i = counters[option].innerHTML
+            counters[option].innerHTML = parseInt(i) + 1
+            },
+        minus: function (option) {
+            let i = counters[option].innerHTML
+            if (i > 0){counters[option].innerHTML = parseInt(i) - 1}
+            },
+        clear: function () {
+            for (let i = 0; i < counters.length; i++){counters[i].innerHTML = 0}
+            clear[0].classList.add("killed")
         },
-        plus: $(),
-        minus:$(),
-        reset:$(),
-        confirm:$()
+        confirm: function () {
+
+        }
     }
-
-
-    $(".dropdown__field").click(function () {dropdown.open()})
+    // Подключение функций
+    for (let i = 0; i < counters.length; i++){
+        plus[i].onclick = function () {dropdown.plus(i); clear[0].classList.remove("killed")}
+        minus[i].onclick = function () {dropdown.minus(i)}
+    }
+    clear[0].onclick = function () {dropdown.clear()}
 })
