9d43d709ca4644ec10a3ec167eaf3b7b2429e4bc
Nikita <nikitakornilov1998@gmail.com>
2021-02-25 08:00:20 +0300

    added new imports

--- src/index.js
+++ src/index.js
@@ -31,9 +31,11 @@ import "/components/form-elements/bullet-list/bullet-list.scss"
 import "/components/form-elements/icon-text-list/icon-text-list.scss"
 import "/components/form-elements/expandable-checkbox/expandable-checkbox.scss"
 import "/components/form-elements/date-dropdown/date-dropdown.scss"
+import "/components/form-elements/masked-text-field/masked-text-field.scss"
 
 // cards css
 import "/components/cards/search-card/search-card.scss"
+import "/components/cards/registration/registration.scss"
 
 
 // js
@@ -43,6 +45,7 @@ import "/components/form-elements/range-slider/range-slider.js"
 import "/components/form-elements/pagination/pagination.js"
 import "/components/form-elements/expandable-checkbox/expandable-checkbox.js"
 import "/components/form-elements/date-dropdown/date-dropdown.js"
+import "/components/form-elements/masked-text-field/masked-text-field.js"
 
 
 
