b574ba35263b277e36e67f5aefc95f8be605a5a3
Nikita <nikitakornilov1998@gmail.com>
2021-03-05 05:30:01 +0300

    added new inputs

--- src/index.js
+++ src/index.js
@@ -35,6 +35,7 @@ import "/components/form-elements/icon-text-list/icon-text-list.scss"
 import "/components/form-elements/expandable-checkbox/expandable-checkbox.scss"
 import "/components/form-elements/date-dropdown/date-dropdown.scss"
 import "/components/form-elements/masked-text-field/masked-text-field.scss"
+import "/components/form-elements/filter-date-dropdown/filter-date-dropdown.scss"
 
 // cards css
 import "/components/cards/search-card/search-card.scss"
@@ -51,6 +52,7 @@ import "/components/form-elements/pagination/pagination.js"
 import "/components/form-elements/expandable-checkbox/expandable-checkbox.js"
 import "/components/form-elements/date-dropdown/date-dropdown.js"
 import "/components/form-elements/masked-text-field/masked-text-field.js"
+import "/components/form-elements/filter-date-dropdown/filter-date-dropdown.js"
 
 
 
