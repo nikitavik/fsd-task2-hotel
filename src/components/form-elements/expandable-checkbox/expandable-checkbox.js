73dd459d12bff780766d6240b719d82c7e9d56dd
Nikita <nikitakornilov1998@gmail.com>
2021-03-09 14:15:46 +0300

    code refactored

--- src/components/form-elements/expandable-checkbox/expandable-checkbox.js
+++ src/components/form-elements/expandable-checkbox/expandable-checkbox.js
@@ -1,5 +1,14 @@
 $(document).ready(function () {
-    $(".expandable-checkbox__marker, .expandable-checkbox__name").click(function (){
-        $(".expandable-checkbox__list").slideToggle()
-    })
+    const marker = $(".expandable-checkbox__marker")
+    $(".expandable-checkbox__marker, .expandable-checkbox__name").click(
+        function (){
+            $(".expandable-checkbox__list").slideToggle()
+
+            if (marker.hasClass("checkbox-expanded")){
+                marker.removeClass("checkbox-expanded")
+            } else {
+                marker.addClass("checkbox-expanded")
+            }
+        })
+
 })
