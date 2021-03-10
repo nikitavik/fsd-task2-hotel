3a9bf27ddd6c9d685dae2be882036054d4567ee7
Nikita <nikitakornilov1998@gmail.com>
2021-03-10 19:21:58 +0300

    made more clickable

--- src/components/form-elements/expandable-checkbox/expandable-checkbox.js
+++ src/components/form-elements/expandable-checkbox/expandable-checkbox.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     const marker = $(".expandable-checkbox__marker")
-    $(".expandable-checkbox__marker, .expandable-checkbox__name").click(
+    $(".expandable-checkbox__label").click(
         function (){
             $(".expandable-checkbox__list").slideToggle()
 
