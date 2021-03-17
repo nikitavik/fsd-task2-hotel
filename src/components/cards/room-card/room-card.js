1dc786942e93a645e8b9345e4adb50a74fd69103
Nikita <nikitakornilov1998@gmail.com>
2021-03-17 03:57:24 +0300

    made a room card

---
+++ src/components/cards/room-card/room-card.js
@@ -0,0 +1,59 @@
+require("lightslider")
+require("lightslider/dist/css/lightslider.css")
+
+$(document).ready(function () {
+    $(".room-card__slideshow").lightSlider({
+            item: 1,
+            autoWidth: false,
+            slideMove: 1, // slidemove will be 1 if loop is true
+            slideMargin: 0,
+
+            addClass: 'room-card-slider',
+            mode: "fade",
+            useCSS: true,
+            cssEasing: 'ease', //'cubic-bezier(0.25, 0, 0.25, 1)',//
+            easing: 'linear', //'for jquery animation',////
+
+            speed: 400, //ms'
+            auto: false,
+            pauseOnHover: false,
+            loop: false,
+            slideEndAnimation: true,
+            pause: 2000,
+
+            keyPress: false,
+            controls: true,
+            nextHtml: "<div class='material-icons room-card__forward' style='color: #FFFFFF;transform: rotate(270deg);'>expand_more</div>",
+            prevHtml: "<div class='material-icons room-card__backward' style='color: #FFFFFF;transform: rotate(90deg);'>expand_more</div>",
+
+
+            rtl: false,
+            adaptiveHeight: false,
+
+            vertical: false,
+            verticalHeight: 500,
+            vThumbWidth: 100,
+
+            thumbItem: 10,
+            pager: true,
+            gallery: false,
+            galleryMargin: 5,
+            thumbMargin: 5,
+            currentPagerPosition: 'right',
+
+            enableTouch: true,
+            enableDrag: true,
+            freeMove: false,
+            swipeThreshold: 40,
+
+            responsive : [],
+
+            onBeforeStart: function (el) {},
+            onSliderLoad: function (el) {},
+            onBeforeSlide: function (el) {},
+            onAfterSlide: function (el) {},
+            onBeforeNextSlide: function (el) {},
+            onBeforePrevSlide: function (el) {}
+        }
+    )
+})
