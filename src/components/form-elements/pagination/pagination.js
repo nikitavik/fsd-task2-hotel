6aa146a8ad2591250b16f3e03788fe7e2ef97369
Nikita <nikitakornilov1998@gmail.com>
2021-03-22 08:38:49 +0300

    (pre push)

--- src/components/form-elements/pagination/pagination.js
+++ src/components/form-elements/pagination/pagination.js
@@ -1,14 +1,39 @@
 require("paginationjs")
+
 $(document).ready(function () {
-    try {
-        $('.pagination-container').pagination({
-            dataSource: [1],
-            autoHidePrevious: true,
-            nextText: "",
-            prevText: "",
-        })
+    const data = $(".room-card")
+
+    function template(data) {
+        let html = "<ul>";
+        $.each(data, function(index, item){
+            html +=  "<li>" + item.parseHTML() + "</li>"
+        });
+        html = html + "</ul>"
+        return html;
     }
-    catch (err){
-        console.log("Pagination not found")
+
+        console.log(template(data))
+
+
+    function log(content){
+        window.console &&
+        console.log(content)
     }
+
+    const container = $(".pagination-container")
+    container.pagination({
+        dataSource: [1,2 ,3],
+        pageSize: 2,
+        autoHidePrevious: true,
+        autoHideNext: true,
+        nextText: "",
+        prevText: "",
+
+        callback: (data, pagination) => {
+            let html = template(data)
+            $(".pagination-data").html(html)
+
+            console.log(html)
+        }
+    })
 })
