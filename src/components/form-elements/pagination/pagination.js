2861f7dd955da809efa922e5ad68d7fe093cc7de
Nikita <nikitakornilov1998@gmail.com>
2021-03-26 19:34:52 +0300

    Temporarily disable pagination

--- src/components/form-elements/pagination/pagination.js
+++ src/components/form-elements/pagination/pagination.js
@@ -1,32 +1,32 @@
-require("paginationjs")
-        require("../../cards/room-card/room-card")
-        $(document).ready(function () {
-            // const data = $(".room-card")
-            //
-            // function template(data) {
-            //     let html = []
-            //     for (let i = 0; i < data.length; i++){
-            //         html.push(data[i])
-            //     }
-            //     return html
-            // }
-
-            const container = $(".pagination-container")
-            const pagination = container.pagination({
-                dataSource: [1,2,3], //template(data)
-                pageSize: 1,
-                autoHidePrevious: true,
-                autoHideNext: true,
-                nextText: "",
-                prevText: "",
-
-                callback: function(data, pagination) {
-                    // const html = template(data);
-                    // $('#data-container').html(html);
-                    // console.log(html)
-                },
-                afterPaging: function () {
-
-                }
-            })
-        })
+// require("paginationjs")
+//         require("../../cards/room-card/room-card")
+//         $(document).ready(function () {
+//             // const data = $(".room-card")
+//             //
+//             // function template(data) {
+//             //     let html = []
+//             //     for (let i = 0; i < data.length; i++){
+//             //         html.push(data[i])
+//             //     }
+//             //     return html
+//             // }
+//
+//             const container = $(".pagination-container")
+//             const pagination = container.pagination({
+//                 dataSource: [1, 2, 3], //template(data)
+//                 pageSize: 1,
+//                 autoHidePrevious: true,
+//                 autoHideNext: true,
+//                 nextText: "",
+//                 prevText: "",
+//
+//                 callback: function(data, pagination) {
+//                     // const html = template(data);
+//                     // $('#data-container').html(html);
+//                     // console.log(html)
+//                 },
+//                 afterPaging: function () {
+//
+//                 }
+//             })
+//         })
